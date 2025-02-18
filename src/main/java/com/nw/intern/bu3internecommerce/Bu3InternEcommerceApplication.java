9eae6625c1d99c8db830003bb78c72b476c24fa4
Tom <nhdoma02@gmail.com>
2025-02-18 17:17:06 +0700

    Initial commit

---
+++ src/main/java/com/nw/intern/bu3internecommerce/Bu3InternEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.nw.intern.bu3internecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bu3InternEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bu3InternEcommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nw/intern/bu3internecommerce/Bu3InternEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nw.intern.bu3internecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bu3InternEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
